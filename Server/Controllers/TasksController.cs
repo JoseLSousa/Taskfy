9c92a43407e579972d43430c3b68ba4dac6b5831
LucasSousa <lucassousasertania@gmail.com>
2024-11-29 19:39:51 -0300

    Feat: Adição dos endpoints Post e Get

---
+++ Server/Controllers/TasksController.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Server.Data;
+using Server.Models;
+
+namespace Server.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TasksController(UserManager<User> userManager, ApplicationDbContext context) : ControllerBase
+    {
+        private readonly UserManager<User> _userManager = userManager;
+        private readonly ApplicationDbContext _context = context;
+
+        [HttpPost]
+        public async Task<IActionResult> AddTask([FromBody] TaskModel taskModel)
+        {
+            if (taskModel == null) return BadRequest("Task data is Invalid");
+
+            var userId = User.Identity?.Name;
+
+            if (userId == null) return Unauthorized("User not authenticated");
+
+            var user = await _userManager.FindByNameAsync(userId);
+            if (user == null) return Unauthorized("User not Found");
+
+            taskModel.UserId = user.Id;
+
+            _context.Tasks.Add(taskModel);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetTask), new { id = taskModel.Id }, taskModel);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetTask(int id)
+        {
+            var task = await _context.Tasks.FindAsync(id);
+
+            if (task == null) return NotFound();
+
+            return Ok(task);
+        }
+    }
+}
