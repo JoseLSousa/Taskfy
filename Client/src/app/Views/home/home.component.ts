2d59d4c7ef2149ffbfd0be63efb6a98623790c60
José Lucas <lucassousasertania@gmail.com>
2024-11-29 12:15:08 -0300

    Style(HomeComponent): Edição estilo HomeComponent

--- Client/src/app/Views/home/home.component.ts
+++ Client/src/app/Views/home/home.component.ts
@@ -1,12 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  currentState: string = 'description'
+  registerForm!: FormGroup
+  loginForm!: FormGroup
 
+
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    })
+
+    this.loginForm = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    })
+  }
+  changeState(state: string): void {
+    this.currentState = state
+  }
 }
