1ee06807d7131c0dd4ef636aa10e2044a2ea6316
LucasSousa <lucassousasertania@gmail.com>
2024-11-29 18:45:26 -0300

    Feat: Configuração da rota dashboard

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './Views/home/home.component';
 import { DashboardComponent } from './Views/dashboard/dashboard.component';
+import { authGuard } from './Guards/auth.guard';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'dashboard', component: DashboardComponent }
+    { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },
 ];
