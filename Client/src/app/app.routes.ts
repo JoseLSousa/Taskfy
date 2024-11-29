f5c17900b78715e9855db648552f0bb593565972
LucasSousa <lucassousasertania@gmail.com>
2024-11-29 14:53:48 -0300

    Feat: Criação do dashboardComponent

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './Views/home/home.component';
+import { DashboardComponent } from './Views/dashboard/dashboard.component';
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent}
+    { path: '', component: HomeComponent },
+    { path: 'dashboard', component: DashboardComponent }
 ];
