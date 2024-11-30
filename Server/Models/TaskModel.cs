0b53536a41406547badb4637d062434c343f521d
LucasSousa <lucassousasertania@gmail.com>
2024-11-30 14:07:44 -0300

    Refactor: Alteração da propriedade DueDate para StartDate e adição da EndDate

--- Server/Models/TaskModel.cs
+++ Server/Models/TaskModel.cs
@@ -11,7 +11,8 @@ namespace Server.Models
         public string Title { get; set; }
         public string Description { get; set; }
         public string Priority { get; set; }
-        public DateTime DueDate { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
         public string UserId { get; set; }
         public User User { get; set; } = null;
     }
