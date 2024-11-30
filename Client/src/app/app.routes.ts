6461ea1e05f3b89d4a5637a55ee8714a8e3d8e5b
LucasSousa <lucassousasertania@gmail.com>
2024-11-30 09:52:33 -0300

    Refactor: Ajuste do novo modelo de rotas

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './Views/home/home.component';
-import { DashboardComponent } from './Views/dashboard/dashboard.component';
+
 import { authGuard } from './Guards/auth.guard';
+import { UpcomingTasksComponent } from './Views/upcoming-tasks/upcoming-tasks.component';
+import { TodayTasksComponent } from './Views/today-tasks/today-tasks.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },
+    {
+        path: 'dashboard',
+        canActivate: [authGuard],
+        children: [
+            { path: "", redirectTo: 'today', pathMatch: 'full' },
+            { path: "today", component: TodayTasksComponent },
+            { path: "upcoming", component: UpcomingTasksComponent }
+        ]
+    },
 ];
