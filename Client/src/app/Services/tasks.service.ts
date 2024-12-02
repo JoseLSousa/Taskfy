d29aa66620dea89eae2c2272c8fe5a8639e90776
LucasSousa <lucassousasertania@gmail.com>
2024-12-02 15:37:40 -0300

    Fix: Alteração do tipo esperado pelo endpoint

--- Client/src/app/Services/tasks.service.ts
+++ Client/src/app/Services/tasks.service.ts
@@ -21,8 +21,8 @@ export class TasksService {
     return this.http.post<Task>(`${this.apiUrl}api/Tasks/add`, data)
   }
 
-  updateTask(task: Task): Observable<Task> {
-    return this.http.put<Task>(`${this.apiUrl}api/Tasks/${task.id}`, task)
+  updateTask(id: any, task: any): Observable<Task> {
+    return this.http.put<Task>(`${this.apiUrl}api/Tasks/update/${id}`, task)
   }
 
   deleteTask(taskId: string): Observable<void> {
