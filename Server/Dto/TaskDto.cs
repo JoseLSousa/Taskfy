85efd6d248a8ad6fd3aa9460c537d6e570d5aa76
LucasSousa <lucassousasertania@gmail.com>
2024-12-02 15:36:54 -0300

    Fix: Adição de propiedades

--- Server/Dto/TaskDto.cs
+++ Server/Dto/TaskDto.cs
@@ -7,11 +7,12 @@ namespace Server.Dto
 {
     public class TaskDto
     {
+        public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public string Priority { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
-
+        public string UserId { get; set; }
     }
 }
