b7e541758bf462c07e1456365afcbbd3a4f3dee9
LucasSousa <lucassousasertania@gmail.com>
2024-12-02 14:36:06 -0300

    Fefactor: Ajuste do código para implementação com sidebar

--- Client/src/app/Views/upcoming-tasks/upcoming-tasks.component.ts
+++ Client/src/app/Views/upcoming-tasks/upcoming-tasks.component.ts
@@ -4,7 +4,6 @@ import { CommonModule } from '@angular/common';
 import { Task } from '../../Interfaces/task';
 import { TasksService } from '../../Services/tasks.service';
 import { TaskSidebarComponent } from "../task-sidebar/task-sidebar.component";
-import { FormBuilder } from '@angular/forms';
 import { addDays, endOfWeek, isWithinInterval, parseISO, startOfDay, startOfWeek } from 'date-fns';
 
 @Component({
@@ -19,20 +18,22 @@ export class UpcomingTasksComponent implements OnInit {
   thisWeekTasks: Task[] = [];
   tomorrowTasks: Task[] = [];
 
-  isSidebarOpen: boolean = false;
+  isSidebarOpen: boolean = false; // Controle de visibilidade da barra lateral
+  selectedTask: Task | null = null; // Tarefa selecionada
+  mode: 'create' | 'edit' | 'view' = 'edit'; // Modo da barra lateral (criação, edição ou visualização)
 
-  constructor(private fb: FormBuilder, private tasksService: TasksService) { }
+  constructor(private tasksService: TasksService) { }
 
   ngOnInit(): void {
     this.fetchTasksThisWeek();
-
   }
 
+  // Busca as tarefas da semana
   fetchTasksThisWeek() {
     this.tasksService.listTasks().subscribe((tasks) => {
-      const today = new Date()
-      const normalizedToday = startOfDay(today)
-      const tomorrow = addDays(normalizedToday, 1)
+      const today = new Date();
+      const normalizedToday = startOfDay(today);
+      const tomorrow = addDays(normalizedToday, 1);
 
       const weekStart = startOfWeek(today, { weekStartsOn: 0 }); // Começo da semana
       const weekEnd = endOfWeek(today, { weekStartsOn: 0 }); // Fim da semana
@@ -51,28 +52,65 @@ export class UpcomingTasksComponent implements OnInit {
 
         if (typeof endDate === "string") endDate = parseISO(endDate);
 
-        const normalizedEndDate = startOfDay(endDate)
+        const normalizedEndDate = startOfDay(endDate);
         return normalizedEndDate.getTime() === normalizedToday.getTime();
       });
 
+      // Filtrando as tarefas que terminam amanhã
       this.tomorrowTasks = tasks.filter((task) => {
         let endDate = task.endDate;
 
         if (typeof endDate === 'string') endDate = parseISO(endDate);
 
-        const normalizedEndDate = startOfDay(endDate)
-
+        const normalizedEndDate = startOfDay(endDate);
         return normalizedEndDate.getTime() === tomorrow.getTime();
-      })
-
+      });
     });
   }
 
-  toggleSidebar() {
-    this.isSidebarOpen = !this.isSidebarOpen
+  // Método para selecionar uma tarefa e abrir a barra lateral
+  selectTaskAndToggleSidebar(task: Task | null) {
+    this.selectTask(task);
   }
 
+  // Método para selecionar uma tarefa (atualiza os dados no formulário e mantém a barra lateral aberta)
+  selectTask(task: Task | null) {
+    this.selectedTask = task;
+    this.mode = task ? 'view' : 'create'; // Muda o modo dependendo se a tarefa existe ou não
+
+    // Se a barra lateral não estiver aberta, abra-a
+    if (!this.isSidebarOpen) {
+      this.isSidebarOpen = true;
+    }
+  }
+
+  // Método para alternar o estado da barra lateral (abrir ou fechar)
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
+    this.fetchTasksThisWeek();
+  }
+
+  // Método para fechar a barra lateral
   handleSidebarClose() {
-    this.isSidebarOpen = false
+    this.isSidebarOpen = false;
+    this.fetchTasksThisWeek();
+  }
+
+  deleteTask(taskId: string | undefined) {
+
+    if (!taskId) return;
+
+    if (confirm('Deseja excluir essa tarefa?')) {
+      this.tasksService.deleteTask(taskId).subscribe({
+        next: () => {
+          alert('Tarefa excluida!')
+          this.handleSidebarClose();
+        },
+        error: (err) => {
+          console.error('Erro ao excluir a tarefa', err)
+        },
+      })
+    }
+    console.log();
   }
 }
